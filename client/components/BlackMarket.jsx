0899c7bc128df26bb737a4c50d36a65e3f281410
ClaireHappel <clairehap1@gmail.com>
2026-07-08 17:38:07 -0500

    create / BlackMarket component + frontend wiring via imports and export

---
+++ client/components/BlackMarket.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function BlackMarket() {
+  return (
+    <Container>
+      <h2>The Black Market</h2>
+    </Container>
+  );
+}
+export default BlackMarket;
